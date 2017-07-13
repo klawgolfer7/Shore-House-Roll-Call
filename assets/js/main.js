4b69ed24df6b4b7133458a788ac161f0a16efe93
klawgolfer7 <kevinrlawler@gmail.com>
2017-07-13 10:45:30 -0400

    initializing firebase to use to track the buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
+console.log("lets get this working!")
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA9f9-swbQH2prnoGjWPhhfuhOnVAB5JyE",
-    authDomain: "shore-house-roll-call-db-9ffed.firebaseapp.com",
-    databaseURL: "https://shore-house-roll-call-db-9ffed.firebaseio.com",
-    projectId: "shore-house-roll-call-db-9ffed",
-    storageBucket: "",
-    messagingSenderId: "359042069125"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA9f9-swbQH2prnoGjWPhhfuhOnVAB5JyE",
+  authDomain: "shore-house-roll-call-db-9ffed.firebaseapp.com",
+  databaseURL: "https://shore-house-roll-call-db-9ffed.firebaseio.com",
+  projectId: "shore-house-roll-call-db-9ffed",
+  storageBucket: "",
+  messagingSenderId: "359042069125"
+};
+firebase.initializeApp(config);
 
 
 window.onload = function () {
