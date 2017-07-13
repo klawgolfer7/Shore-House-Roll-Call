10adbea410ee01d8f5ca3d7bde36e8d3c41221a7
klawgolfer7 <kevinrlawler@gmail.com>
2017-07-13 10:43:53 -0400

    initializing firebase to use to track the buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA9f9-swbQH2prnoGjWPhhfuhOnVAB5JyE",
+    authDomain: "shore-house-roll-call-db-9ffed.firebaseapp.com",
+    databaseURL: "https://shore-house-roll-call-db-9ffed.firebaseio.com",
+    projectId: "shore-house-roll-call-db-9ffed",
+    storageBucket: "",
+    messagingSenderId: "359042069125"
+  };
+  firebase.initializeApp(config);
+
+
 window.onload = function () {
+
+var d = new Date();
 
 document.getElementById("demo").innerHTML = Date();
 };
+
+
