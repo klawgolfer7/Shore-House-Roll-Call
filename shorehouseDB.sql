0f9d6bfc9c902bfeed9882a7fae0a155ab752a01
klawgolfer7 <kevinrlawler@gmail.com>
2017-09-14 17:45:21 -0400

    adding the start of the SQL database

---
+++ shorehouseDB.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE shorehouse_db;
+
+USE shorehouse_db;
+
+CREATE TABLE people (
+	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+
+	name VARCHAR(40) NOT NULL,
+
+	is_coming BOOLEAN NOT NULL,
+);
+
+INSERT INTO people (name, is_coming)
+VALUES ("Kevin", TRUE);
+
+INSERT INTO people (name, is_coming)
+VALUES ("Kelly", TRUE);
+
